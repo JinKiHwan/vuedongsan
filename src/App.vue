b8dc130ba416bb680673e0851f130d12445629b2
Jin KiHwan <eljsh95@naver.com>
2024-01-18 00:36:21 +0900

    import,export 수업

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,21 @@
 
   <div @click="모달창열렸니 = true">
     <img src="./assets/room0.jpg" alt="" class="room-img" />
-    <h4>{{ products[0] }}</h4>
+    <h4>{{ 원룸들[0].title }}</h4>
     <p>{{ price1 }} 만원</p>
     <button @:click="신고수[0]++">허위매물신고</button>
     <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
     <img src="./assets/room1.jpg" alt="" class="room-img" />
-    <h4>{{ products[1] }}</h4>
+    <h4>{{ 원룸들[1].title }}</h4>
     <p>{{ price2 }} 만원</p>
     <button @:click="신고수[1]++">허위매물신고</button>
     <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
     <img src="./assets/room2.jpg" alt="" class="room-img" />
-    <h4>{{ products[2] }}</h4>
+    <h4>{{ 원룸들[2].title }}</h4>
     <p>XX 만원</p>
     <button @:click="신고수[2]++">허위매물신고</button>
     <span>신고수 : {{ 신고수[2] }}</span>
@@ -35,10 +35,13 @@
 </template>
 
 <script>
+import oneroom from './assets/oneroom';
+
 export default {
   name: 'App',
   data() {
     return {
+      원룸들: oneroom,
       모달창열렸니: false,
       메뉴들: ['Home', 'Products', 'About'],
       price1: 60,

7654e37e7919516db82aabecfe70409b8d0755ef
Jin KiHwan <eljsh95@naver.com>
2024-01-17 17:52:04 +0900

    버튼 숙제

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,19 @@
     <h4>{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
     <button @:click="increase">허위매물신고</button>
-    <span>신고수 : {{ 신고수 }}</span>
+    <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
     <p>{{ price2 }} 만원</p>
-    <button @:click="increase">허위매물신고</button>
-    <span>신고수 : {{ 신고수 }}</span>
+    <button @:click="increase1">허위매물신고</button>
+    <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
     <h4>{{ products[2] }}</h4>
     <p>XX 만원</p>
-    <button @:click="increase">허위매물신고</button>
-    <span>신고수 : {{ 신고수 }}</span>
+    <button @:click="increase2">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }}</span>
   </div>
 </template>
 
@@ -32,13 +32,19 @@ export default {
       price1: 60,
       price2: 70,
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
-      신고수: 0,
+      신고수: [0, 0, 0],
     };
   },
 
   methods: {
     increase() {
-      this.신고수++;
+      this.신고수[0]++;
+    },
+    increase1() {
+      this.신고수[1]++;
+    },
+    increase2() {
+      this.신고수[2]++;
     },
   },
 

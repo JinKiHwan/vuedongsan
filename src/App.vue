fc9b0ea20b0c55e217f82daad6c3180046859538
Jin KiHwan <eljsh95@naver.com>
2024-01-17 17:43:15 +0900

    이벤트 핸들러

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,20 @@
   <div>
     <h4>{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
+    <button @:click="increase">허위매물신고</button>
+    <span>신고수 : {{ 신고수 }}</span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
     <p>{{ price2 }} 만원</p>
+    <button @:click="increase">허위매물신고</button>
+    <span>신고수 : {{ 신고수 }}</span>
   </div>
   <div>
     <h4>{{ products[2] }}</h4>
     <p>XX 만원</p>
+    <button @:click="increase">허위매물신고</button>
+    <span>신고수 : {{ 신고수 }}</span>
   </div>
 </template>
 
@@ -26,8 +32,16 @@ export default {
       price1: 60,
       price2: 70,
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      신고수: 0,
     };
   },
+
+  methods: {
+    increase() {
+      this.신고수++;
+    },
+  },
+
   components: {},
 };
 </script>

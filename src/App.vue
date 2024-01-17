d0eb8d7f294eb7217078e8ce439b194920ad396e
Jin KiHwan <eljsh95@naver.com>
2024-01-17 17:54:05 +0900

    버튼추가

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #000;
+  color: #001;
   text-align: center;
 }
 

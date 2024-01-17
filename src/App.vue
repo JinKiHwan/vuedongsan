b89068a403a10620bb2a04febecb18f421fba9cc
Jin KiHwan <eljsh95@naver.com>
2024-01-17 18:14:24 +0900

    if문

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,33 @@
     <a v-for="(a, i) in 메뉴들" :key="i">{{ a }}</a>
   </div>
 
-  <div>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button>닫기</button>
+    </div>
+  </div>
+
+  <div @click="모달창열렸니 = true">
+    <img src="./assets/room0.jpg" alt="" class="room-img" />
     <h4>{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
-    <button @:click="increase">허위매물신고</button>
+    <button @:click="신고수[0]++">허위매물신고</button>
     <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" alt="" class="room-img" />
     <h4>{{ products[1] }}</h4>
     <p>{{ price2 }} 만원</p>
-    <button @:click="increase1">허위매물신고</button>
+    <button @:click="신고수[1]++">허위매물신고</button>
     <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" alt="" class="room-img" />
     <h4>{{ products[2] }}</h4>
     <p>XX 만원</p>
-    <button @:click="increase2">허위매물신고</button>
+    <button @:click="신고수[2]++">허위매물신고</button>
     <span>신고수 : {{ 신고수[2] }}</span>
   </div>
 </template>
@@ -28,6 +39,7 @@ export default {
   name: 'App',
   data() {
     return {
+      모달창열렸니: false,
       메뉴들: ['Home', 'Products', 'About'],
       price1: 60,
       price2: 70,
@@ -36,17 +48,7 @@ export default {
     };
   },
 
-  methods: {
-    increase() {
-      this.신고수[0]++;
-    },
-    increase1() {
-      this.신고수[1]++;
-    },
-    increase2() {
-      this.신고수[2]++;
-    },
-  },
+  methods: {},
 
   components: {},
 };
@@ -72,5 +74,29 @@ export default {
   padding: 10px;
   text-decoration: none;
   cursor: pointer;
+}
+.room-img {
+  width: 100%;
+  margin-top: 50px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
